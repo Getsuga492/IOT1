5ef51ce6a7da846f0bc9aa5721c6f9ad0c34a3c0
Getsuga492 <vegito218@icloud.com>
2025-04-03 15:34:24 -0400

    functional part 4

--- week_9/app.py
+++ week_9/app.py
@@ -1,33 +1,27 @@
 import json
 import time
 import paho.mqtt.client as mqtt
-from gpiozero import LED
 
 id = 'ea16da5a-1155-4a0f-9e69-3283fa2413c9'
 
-led = LED(26)
-
 client_telemetry_topic = id + '/telemetry'
 server_command_topic = id + '/commands'
-client_name = id + 'temperature_server'
+client_name = id + '_temperature_server'
 
 mqtt_client = mqtt.Client(client_name)
 mqtt_client.connect('test.mosquitto.org')
 
 mqtt_client.loop_start()
 
+# Function to process telemetry and send LED command
 def handle_telemetry(client, userdata, message):
     payload = json.loads(message.payload.decode())
     print("Message received:", payload)
-    command = { 'led_on' : payload['temperature'] > 25 }
-    print("Sending message:", command)
-
-    if payload['temperature'] > 25:
-        led.on()
-    else:
-        led.off()
-
-    client.publish(server_command_topic, json.dumps(command))
+    
+    command = { 'led_on': payload['temperature'] > 27 }  # Logic for LED
+    print("Sending command:", command)
+    
+    client.publish(server_command_topic, json.dumps(command))  # Send command
 
 mqtt_client.subscribe(client_telemetry_topic)
 mqtt_client.on_message = handle_telemetry
--- week_9/mqtt_temp_2.py
+++ week_9/mqtt_temp_2.py
@@ -1,35 +1,49 @@
-from gpiozero import LED
 import time
-import random
 import paho.mqtt.client as mqtt
 import json
+from gpiozero import LED
 
-led = LED(26)
-id = 'ea16da5a-1155-4a0f-9e69-3283fa2413c9' #replace with your own unique id
+id = 'ea16da5a-1155-4a0f-9e69-3283fa2413c9'  # Replace with your own unique ID
+
 client_telemetry_topic = id + '/telemetry'
+server_command_topic = id + '/commands'
 client_name = id + '_temperature_client'
+
+led = LED(26) 
 
 mqtt_client = mqtt.Client(client_name)
 mqtt_client.connect('test.mosquitto.org')
 
-def ds18b20Read():
-	tfile = open("/sys/bus/w1/devices/28-3c01b556ec19/w1_slave")
-	text = tfile.read()
-	tfile.close()
-	secondline = text.split("\n")[1]
-	temperaturedata = secondline.split(" ")[9]
-	temperature = float(temperaturedata[2:])
-	temperature = temperature / 1000
-	temperature = round(temperature, 2)
-	return temperature
+# Function to handle incoming commands
+def handle_command(client, userdata, message):
+    payload = json.loads(message.payload.decode())
+    print("Command received:", payload)
 
+    if payload.get("led_on"): 
+        led.on()
+    else:
+        led.off()
+
+# Read temperature from the sensor
+def ds18b20Read():
+    tfile = open("/sys/bus/w1/devices/28-3c01b556ec19/w1_slave")
+    text = tfile.read()
+    tfile.close()
+    secondline = text.split("\n")[1]
+    temperaturedata = secondline.split(" ")[9]
+    temperature = float(temperaturedata[2:]) / 1000
+    return round(temperature, 2)
+
+# Subscribe to receive commands
+mqtt_client.subscribe(server_command_topic)
+mqtt_client.on_message = handle_command  # Assign message handler
 mqtt_client.loop_start()
 
 print("MQTT connected!")
 
 while True:
     temperature = ds18b20Read()
-    telemetry = json.dumps({'temperature' : temperature})
-    print("Sending telemetry ", telemetry)
+    telemetry = json.dumps({'temperature': temperature})
+    print("Sending telemetry:", telemetry)
     mqtt_client.publish(client_telemetry_topic, telemetry)
     time.sleep(5)
